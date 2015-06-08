c3e585e0bfc8fbc3bf79fdbb7075b879411b8ded
unknown <chazly321@gmail.com>
2015-06-08 14:29:14 -0700

    added local correctly..

--- ProjectSite/settings/local.py
+++ ProjectSite/settings/local.py
@@ -112,8 +112,8 @@ DATABASES = {
     'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
     'PORT': '',                      # Set to empty string for default.
     }
-}   
-
+}  
+ 
 
 
 # Internationalization
