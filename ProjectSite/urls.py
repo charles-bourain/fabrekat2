857762b425186a9a523120504371d0fefc119306
unknown <chazly321@gmail.com>
2016-03-29 08:58:12 -0700

    settings changes..

--- ProjectSite/urls.py
+++ ProjectSite/urls.py
@@ -26,7 +26,3 @@ urlpatterns = patterns('',
 if settings.DEBUG == True:
     urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
