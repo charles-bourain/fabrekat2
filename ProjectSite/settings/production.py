7192a15f3a7ec819e08efa9b888a6e7cb3f199c2
unknown <chazly321@gmail.com>
2016-03-29 08:42:23 -0700

    Changed to Whitenoise in prod settings

--- ProjectSite/settings/production.py
+++ ProjectSite/settings/production.py
@@ -20,8 +20,8 @@ DATABASES = {
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
