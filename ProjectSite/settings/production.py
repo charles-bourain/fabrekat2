e7fb16412437beadaace17ff15f54ec07a90b547
unknown <chazly321@gmail.com>
2016-04-09 11:15:03 -0700

    Secret Key TS..

--- ProjectSite/settings/production.py
+++ ProjectSite/settings/production.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-SECRET_KEY = os.environ('SECRET_KEY')
-AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 #PRODUCTION SPECIFIC APPS
 base.INSTALLED_APPS += [
