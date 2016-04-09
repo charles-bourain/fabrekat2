d2ea7d93d3a959101b73183b7a47e81fa46f4b80
unknown <chazly321@gmail.com>
2016-04-09 11:39:31 -0700

    Error in default file storage string

--- ProjectSite/settings/production.py
+++ ProjectSite/settings/production.py
@@ -64,7 +64,7 @@ TEMPLATE_DIRS=(
     )
 
 #MEDIA AND MEDIA STORAGE MANAGEMENT
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoSTorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 MEDIA_ROOT = os.path.join(base.BASE_DIR, 'media')
 MEDIA_URL = 'http://%s.s3-website-us-west-2.amazonaws.com/' % base.AWS_STORAGE_BUCKET_NAME
 
